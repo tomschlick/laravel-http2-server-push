255968103b60164e1ca9b48dc16c8b5d1f61b5fd
Tom Schlick <tom@tomschlick.com>
2017-02-07 23:46:00 -0500

    styleci alignment

--- config/server-push.php
+++ config/server-push.php
@@ -21,10 +21,10 @@ return [
     'autolink_from_manifest' => true,
 
     // Elixir example
-    'manifest_path' => public_path('build/rev-manifest.json'),
+    'manifest_path'   => public_path('build/rev-manifest.json'),
     'assets_base_uri' => '/build/',
 
     // Mix example
-    //'manifest_path' => public_path('mix-manifest.json'),
+    //'manifest_path'   => public_path('mix-manifest.json'),
     //'assets_base_uri' => '/',
 ];
